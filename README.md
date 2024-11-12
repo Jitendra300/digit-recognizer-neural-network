eac25bec1d3c694304e81a4d5721709a8c7c27f3
Stephen <jitendrasundesha@gmail.com>
2024-11-12 10:27:33 +0530

    Adding Command Arguments and ML terms into Project with Updating README and creating requirements.txt

--- README.md
+++ README.md
@@ -1,30 +1,37 @@
 # Digit Recognizer in Python from scratch
-This project aims to develop a neural network from scratch (only using numpy)!
+This project aims to develop a neural network from scratch [no PyTorch, no TensorFlow!!]
     
 # Why do it?
 To get a deeper knowledge of neural networks and also cause its FUN!!!
     
 # Neural Architecture
-First we have one input layer which consists of **64** inputs and then we have a hidden layer of **10** neurons and then at last we have output layer of **10** neurons.
+First we have one input layer which consists of **64** inputs and then we have a hidden layer of **10** neurons and then at last we have output layer of **10** neurons. We have default epoch size of 65, batchsize of 8, learningrate[alpha] of 0.03 and datasplit of 1000. We can change this default values from command arguments of the file.
 
 # Libraries Used:
 - numpy
 - sklearn
 
+# Installation:
+    git clone https://github.com/Jitendra300/digit-recognizer-neural-network.git
+    cd digit-recognizer-neural-network
+    pip install -r requirements.txt
+    python main.py
+
+# Demo:
+    python main.py --epoch=50 --learning_rate=0.01 --batch_size=16 --datasplit=1200
+    
 # Output:
-By keeping the training iterations at **5000** and learning rate (alpha) at **0.01** we saw that our model is nearly **85%** correct at predicting.<br><br>
-![Output 1](/images/minimalOutcome.png)
+By keeping the default values namely epoch=65, learning_rate=0.03, batch_size=8, datasplit=1000 we get an accuracy of 92.8481
+![Output 1](/images/output.png)
 <br><br>
-By increasing the training iterations to **20000** we nearly achieve **92%** correction on testing data. <br><br>
-![Output 2](/images/currentMaxOutcome.png)
-<br>
+
 # More Info:
 Am I cheating by including numpy when this project was supposed to be done from scratch? Well...good question! But how can we ignore the beautiful numpy??? Maybe it can be a future project to do it fully from scratch ;) <br><br>
 Also we are using sklearn cause we need the dataset which includes both image and label. Each image is of **8x8** pixels. <br><br>
 For more info check the code or message me!
 
 # Limitations:
-Cause this is a fun project and my only aim is to learn neural networks by playing around, I don't have any intentions to make this neural network perform at its best, and also one limitations I have noticed is that the neural network doesn't get anybetter than ***94%***.<br><br>
+Cause this is a fun project and my only aim is to learn neural networks by playing around, I don't have any intentions to make this neural network perform at its best, and also one limitations I have noticed is that the neural network doesn't get anybetter than ***93%***.<br><br>
 This maybe due to the network architecture or cause this project is from scratch or maybe cause the neural network has found a local minima instead of global minima. Maybe all this can be overcome if we use different neural architecture, use different activation function, use more neurons in layers or increase the amount of hidden layers or maybe have bigger dataset.<br><br>
 We might overcome this challenges in future projects
 
--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import math
+import argparse
 import numpy as np
 from sklearn.datasets import load_digits
 
+# Creating the parser for command arguments
+parser = argparse.ArgumentParser(description='Malware Analysis Program')
+# Add arguments
+parser.add_argument('--batch_size', type=int, default=8, help='The batch size of the training dataset. Default = 8')
+parser.add_argument('--learning_rate', type=float, default=0.03, help='Learning rate (alpha) of our model. Default = 0.03')
+parser.add_argument('--epoch', type=int, default=65, help='Epoch size. Default = 65')
+parser.add_argument('--datasplit', type=int, default=1000, help='Data split between testing and training. Default = 1000. Nearly 60/40 distribution.')
+
+# Parse the arguments
+args = parser.parse_args()
+
+
+# creating minibatches
+def miniBatch(A, batchSize):
+    numObs = A.shape[0]
+    batches = []
+    batchNum = math.floor(numObs / batchSize)
+
+    if(numObs%batchSize == 0):
+        for i in range(batchNum):
+            aBatch = A[i * batchSize:(i + 1) * batchSize, :]
+            batches.append(aBatch)
+    else:
+        for i in range(batchNum):
+            aBatch = A[i * batchSize:(i + 1) * batchSize, :]
+            batches.append(aBatch)
+        aBatch = A[batchNum * batchSize:, :]
+        batches.append(aBatch)
+    return batches
 
 # this is onehotencoding
 def onehotencoding(arr):
@@ -71,44 +102,52 @@ def predictionAccuracy(w1, w2, b1, b2, xTest, yTest):
         totalScore+=1
     print("Accuracy: ",(100*score/totalScore))
 
-def gradientDescient(X, Y, total_iteration, learningRate):
+def gradientDescient(X, Y, epoch, learningRate):
     w1, w2, b1, b2 = initializingParameters()
-    for i in range(total_iteration):
-        z1, z2, a1, a2 = forwardPropagation(w1, w2, b1, b2, X)
-        dw1, dw2, db1, db2 = backwardPropagation(w1, w2,z1, a1, a2, X, Y)
-        w1, w2, b1, b2 = updateParams(w1, w2, b1, b2, dw1, dw2, db1, db2, learningRate)
+    for i in range(epoch):
+        for j in range(len(X)):
+            # taking transpose of it so that dimensions match while dot product
+            current_X_training_dataset = np.array(X[j]).T
+            current_Y_training_dataset = np.array(Y[j]).T
+            z1, z2, a1, a2 = forwardPropagation(w1, w2, b1, b2, current_X_training_dataset)
+            dw1, dw2, db1, db2 = backwardPropagation(w1, w2,z1, a1, a2, current_X_training_dataset, current_Y_training_dataset)
+            w1, w2, b1, b2 = updateParams(w1, w2, b1, b2, dw1, dw2, db1, db2, learningRate)
         # checks how good our  model is doing for every iteration of 100
-        if(i%100 == 0):
-            print("Iteration: ", i//100)
-            predictionAccuracy(w1, w2, b1, b2, xTest, yTest)
+        print("Epoch : ", i)
+        predictionAccuracy(w1, w2, b1, b2, xTest, yTest)
     return w1, w2, b1, b2
 
+
 # Variables are declared here...
-learningRate = 0.01 # also known as alpha
-dataSplit = 1000 # this indicates that first 1000 would be trainingdata and other would be testdata
+learningRate = args.learning_rate # also known as alpha
+dataSplit = args.datasplit # this indicates that first 1000 would be trainingdata and other would be testdata
+batchSize = args.batch_size
+epoch = args.epoch
 
 """
 this includes data of images and labels
 where each image is of a single digit number which has dimensions 8x8,
 and label of each image gives the digit number.
  """
+
 data = load_digits()
 imagesData = data["data"]
 labelData = data["target"]
 
 # Training data
-xTrain = imagesData[:dataSplit]
-yTrain = np.array(onehotencoding(labelData[:dataSplit]))
+whole_xTrain = imagesData[:dataSplit]
+whole_yTrain = np.array(onehotencoding(labelData[:dataSplit]))
+
 #  Testing data
 xTest = imagesData[dataSplit:]
 yTest  = np.array(onehotencoding(labelData[dataSplit:]))
 
-# taking a transpose of the below arrays so that each column is an input
-xTrain = xTrain.T
-yTrain = yTrain.T
+xTrain = miniBatch(whole_xTrain, batchSize)
+yTrain = miniBatch(whole_yTrain, batchSize)
 
-w1, w2, b1, b2 = gradientDescient(xTrain, yTrain, 10000, learningRate)
+w1, w2, b1, b2 = gradientDescient(xTrain, yTrain, epoch, learningRate)
 
 print("------------------------------------\n")
 print("Final Results: ")
+print("Testing on Test Datasets: ")
 predictionAccuracy(w1, w2, b1, b2, xTest, yTest)
